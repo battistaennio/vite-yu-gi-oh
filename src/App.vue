2b2f0ae9a4501b29ab653952c8550b6797c736d5
battistaennio <ennio.battista99@gmail.com>
2024-07-09 15:10:50 +0200

    feat: add main and its style

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,23 @@ export default{
 </script>
 
 <template>
+
   <AppHeader />
+
+  <main>
+
+  </main>
+
 </template>
 
 <style lang="scss">
 @use "./style/general.scss";
+@use "./style/partials/variables" as *;
+
+main{
+  background-color: $secondary-color;
+padding: 40px;
+  margin: 30px 100px;
+  min-height: 200px;
+}
 </style>
