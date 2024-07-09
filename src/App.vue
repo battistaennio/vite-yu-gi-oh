83eb925b0b26445a6cf591087fc67b2fc87a8c4d
battistaennio <ennio.battista99@gmail.com>
2024-07-09 15:04:37 +0200

    feat: add AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 
+export default{
+  name: "app",
+  components: {
+    AppHeader,
+
+  }
+}
 </script>
 
 <template>
-
+  <AppHeader />
 </template>
 
-<style>
-
+<style lang="scss">
+@use "./style/general.scss";
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<template>
+    <header>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg/1200px-Yu-Gi-Oh%21_%28Logo%29.jpg" alt="Yu-Gi-Oh logo">
+        <h1>Yu-Gi-Oh API</h1>
+    </header>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/variables" as *;
+
+    header{
+        text-align: center;
+        background-color: $secondary-color;
+        padding: 20px 0;
+    }
+
+    h1{
+        font-weight: 500;
+        display: inline-block;
+    }
+
+    img{
+        width: 5%;
+    }
+</style>
